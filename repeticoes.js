b7874276ddd92e78a2a270d318fd6f54f24400d4
FIC <fic@info.local>
2025-04-07 21:47:59 -0300

    exercicio-condicional-finalizado

---
+++ repeticoes.js
@@ -0,0 +1,44 @@
+function exemplofor() {
+
+    //FOR
+    // 1 declarar contador
+    // 2 condição
+    // 3 incremento
+    for (let contador = 1; contador <= 5; contador++) {
+        alert(contador)
+
+    }
+
+}
+
+function exemplowhile() {
+
+    let contador = 1
+
+    while (contador <= 5) {
+
+        console.log(contador);
+        contador++;
+    }
+
+}
+
+
+function exemploDowhile() {
+
+    let contador = 1;
+    do{
+        alert( contador);
+        contador++;
+    } while (contador <=5);
+}
+
+
+
+function exercicio1
+
+for ( let contador = 1; contador <= 10; contador++) { 
+    alert (contador)
+} 
+
+exercicio1 ()
